55be4866fd29cf0a6fd404ebf20371ca80e8710c
Jonathan Yuen <jonwyuen@gmail.com>
2017-11-04 02:46:54 -0700

    separated todos route logic into a helpers file

--- todo-api/routes/todos.js
+++ todo-api/routes/todos.js
@@ -1,62 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const db = require("../models");
+const helpers = require("../helpers/todos");
 
-router.get("/", function(req, res) {
-  db.Todo
-    .find()
-    .then(function(todos) {
-      res.json(todos);
-    })
-    .catch(function(err) {
-      res.send(err);
-    });
-});
+router
+  .route("/")
+  .get(helpers.getTodos)
+  .post(helpers.createTodo);
 
-router.post("/", function(req, res) {
-  db.Todo
-    .create(req.body)
-    .then(function(newTodo) {
-      res.status(201).json(newTodo);
-    })
-    .catch(function(err) {
-      res.send(err);
-    });
-});
-
-router.get("/:todoId", function(req, res) {
-  db.Todo
-    .findById(req.params.todoId)
-    .then(function(foundTodo) {
-      res.json(foundTodo);
-    })
-    .catch(function(err) {
-      res.send(err);
-    });
-});
-
-router.put("/:todoId", function(req, res) {
-  // db.Todo.findOneAndUpdate({_id: req.params.todoId}, req.body, {new: true})
-  db.Todo
-    .findByIdAndUpdate(req.params.todoId, req.body, { new: true })
-    .then(function(todo) {
-      res.json(todo);
-    })
-    .catch(function(err) {
-      res.send(err);
-    });
-});
-
-router.delete("/:todoId", function(req, res) {
-  // db.Todo.remove({_id: req.params.todoId})
-  db.Todo
-    .findByIdAndRemove(req.params.todoId)
-    .then(function() {
-      res.json({ message: "Todo Deleted" });
-    })
-    .catch(function(err) {
-      res.send(err);
-    });
-});
+router
+  .route("/:todoId")
+  .get(helpers.getTodo)
+  .put(helpers.updateTodo)
+  .delete(helpers.deleteTodo);
 
 module.exports = router;
