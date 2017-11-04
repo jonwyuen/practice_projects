bb53fee91e8b4cbe053c8aac585ffad92d209b41
Jonathan Yuen <jonwyuen@gmail.com>
2017-11-04 00:17:06 -0700

    added routes

--- todo-api/index.js
+++ todo-api/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
 
+const todoRoutes = require("./routes/todos");
+
 app.get("/", function(req, res) {
   res.json({ message: "Todo App" });
 });
+
+app.use("/api/todos", todoRoutes);
 
 app.listen(3001, function() {
   console.log("Server is listening on port 3001");
