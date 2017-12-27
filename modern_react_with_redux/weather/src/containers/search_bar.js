1ac8d830ff12a4860cc8bde3608014bd239a3d98
Jonathan Yuen <jonwyuen@gmail.com>
2017-12-27 02:46:53 -0800

    adding weather list

--- modern_react_with_redux/weather/src/containers/search_bar.js
+++ modern_react_with_redux/weather/src/containers/search_bar.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+  }
+
+  onInputChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  onFormSubmit(e) {
+    e.preventDefault();
+
+    this.props.fetchWeather(this.state.term);
+    this.setState({ term: '' });
+  }
+
   render() {
     return (
-      <form className="input-group">
-        <input type="text" />
+      <form className="input-group" onSubmit={this.onFormSubmit}>
+        <input
+          type="text"
+          placeholder="Get a five-day forecast in your favorite cites"
+          className="form-control"
+          name="term"
+          value={this.state.term}
+          onChange={this.onInputChange}
+        />
         <span className="input-group-btn">
           <button type="submit" className="btn btn-secondary">
             Submit
@@ -15,4 +44,8 @@ class SearchBar extends Component {
   }
 }
 
-export default SearchBar;
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
--- modern_react_with_redux/weather/src/index.js
+++ modern_react_with_redux/weather/src/index.js
@@ -2,14 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import ReduxPromise from 'redux-promise';
+// if action has promise as payload, stop action
+// after promise resolves, create a new action and send it to reducers
 
 import App from './components/app';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <App />
-  </Provider>
-  , document.querySelector('.container'));
+  </Provider>,
+  document.querySelector('.container')
+);
