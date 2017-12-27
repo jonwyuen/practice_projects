1d26911362a89f968055d5f5350a2418aec1b4fd
Jonathan Yuen <jonwyuen@gmail.com>
2017-12-27 04:57:04 -0800

    finished weather app

---
+++ modern_react_with_redux/weather/src/components/chart.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+  Sparklines,
+  SparklinesLine,
+  SparklinesReferenceLine
+} from 'react-sparklines';
+
+function average(data) {
+  return Math.round(data.reduce((a, b) => a + b) / data.length);
+}
+
+const Chart = props => (
+  <div>
+    <Sparklines height={120} width={180} data={props.data}>
+      <SparklinesLine color={props.color} />
+      <SparklinesReferenceLine type="avg" />
+    </Sparklines>
+    <div>
+      {average(props.data)} {props.units}
+    </div>
+  </div>
+);
+
+export default Chart;
--- modern_react_with_redux/weather/src/containers/weather_list.js
+++ modern_react_with_redux/weather/src/containers/weather_list.js
@@ -1,19 +1,48 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Chart from '../components/chart';
+import Map from '../components/map';
 
 class WeatherList extends Component {
+  renderWeather(cityData) {
+    const name = cityData.city.name;
+    const temps = cityData.list.map(weather => weather.main.temp);
+    const pressures = cityData.list.map(weather => weather.main.pressure);
+    const humidities = cityData.list.map(weather => weather.main.humidity);
+    const { lat, lon } = cityData.city.coord;
+
+    return (
+      <tr key={name}>
+        <td>
+          <Map lat={lat} lon={lon} />
+        </td>
+        <td>
+          <Chart data={temps} color="blue" units="K" />
+        </td>
+        <td>
+          <Chart data={pressures} color="red" units="hPa" />
+        </td>
+        <td>
+          <Chart data={humidities} color="green" units="%" />
+        </td>
+      </tr>
+    );
+  }
+
   render() {
+    const weatherList = this.props.weather.map(this.renderWeather);
+
     return (
       <table className="table table-hover">
         <thead>
           <tr>
             <th>City</th>
-            <th>Temperature</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
+            <th>Temperature (K)</th>
+            <th>Pressure (hPa)</th>
+            <th>Humidity (%)</th>
           </tr>
         </thead>
-        <tbody />
+        <tbody>{weatherList}</tbody>
       </table>
     );
   }
