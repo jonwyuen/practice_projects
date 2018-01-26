cbdf9f55b3289b8c26bccd3c66fdb91400b12d40
Jonathan Yuen <jonwyuen@gmail.com>
2018-01-26 01:08:57 -0800

    url shortener prompt

---
+++ url_shortener/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+// Express config
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'html');
+app.engine('html', require('ejs').renderFile);
+
+// Routes
+app.get('/', function (req, res) {
+  res.render('index');
+});
+
+app.post('/create', function createShortenedURL(req, res) {
+  // TODO: Implement me
+  res.render('index');
+});
+
+app.get('/:urlAlias', function redirectToOriginalURL(req, res) {
+  // GET http://127.0.0.1/<url_alias>
+  //  => http://example.org/original/url
+
+  res.status(200).send('TODO: Implement me');
+});
+
+// Start server
+app.listen(3000, function () {
+  console.log('Listening on port 3000.');
+});
