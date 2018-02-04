c6ff799699afd0c4ffe92b896b3fbcf723f9fe57
Jonathan Yuen <jonwyuen@gmail.com>
2018-02-04 03:29:26 -0800

    carousel skeleton

---
+++ carousel/src/Carousel.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Carousel.css';
+
+class Carousel extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <div></div>
+    )
+  }
+}
+
+export default Carousel;
