abaa0fe78a11926a1358ec950f962960a94c6956
Jonathan Yuen <jonwyuen@gmail.com>
2018-02-06 05:21:41 -0800

    setting up carousel

--- carousel/src/containers/Carousel.js
+++ carousel/src/containers/Carousel.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
+import Slide from '../components/Slide';
+import SliderButton from '../components/SliderButton';
 import './Carousel.css';
 
 class Carousel extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      slides: ['yellow', 'blue', 'green', 'red', 'orange'],
+      current: 0,
+      next: 0,
+      canSlide: true,
+    }
   }
 
   render() {
