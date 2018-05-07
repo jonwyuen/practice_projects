e168d6b5c8891f7b62129139cf003035c10a2938
Jonathan Yuen <jonwyuen@gmail.com>
2018-05-07 02:23:03 -0700

    auth hoc

---
+++ react_redux/auth_hoc/src/components/resources.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Resources = () => {
+  return (
+    <div>
+      Secret
+      <ul>
+        <li>S1</li>
+        <li>S2</li>
+        <li>S3</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Resources;
