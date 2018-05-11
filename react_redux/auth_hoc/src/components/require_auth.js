4407569f3886a558c70e4c7a86d80d810ca92f09
Jonathan Yuen <jonwyuen@gmail.com>
2018-05-11 00:17:30 -0700

    protected route

--- react_redux/auth_hoc/src/components/require_auth.js
+++ react_redux/auth_hoc/src/components/require_auth.js
@@ -3,6 +3,18 @@ import { connect } from "react-redux";
 
 export default function(ComposedComponent) {
   class Auth extends Component {
+    static contextTypes = {
+      router: React.PropTypes.object
+    };
+
+    componentWillMount() {
+      if (!this.props.authenticated) this.context.router.push("/");
+    }
+
+    componentWillUpdate(nextProps) {
+      if (!nextProps.authenticated) this.context.router.push("/");
+    }
+
     render() {
       return <ComposedComponent {...this.props} />;
     }
