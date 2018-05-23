ce875ec079dbfc4f940b173cad24299576896dbc
Jonathan Yuen <jonwyuen@gmail.com>
2018-05-23 00:46:57 -0700

    jwt strategy

--- react_redux/auth/server/services/passport.js
+++ react_redux/auth/server/services/passport.js
@@ -3,3 +3,20 @@ const User = require("../models/user");
 const config = require("../config");
 const JwtStragegy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
+
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromHeader("authorization"),
+  secretOrKey: config.secret
+};
+
+const jwtLogin = new JwtStragegy(jwtOptions, function(payload, done) {
+  // see if user id in payload exists in db
+  // if yes, call done with that user, else call done w/o user obj
+  User.findById(payload.sub, function(err, user) {
+    if (err) return done(err, false);
+    if (user) done(null, user);
+    else done(null, false);
+  });
+});
+
+passport.use(jwtLogin);
