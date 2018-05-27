8d375e5959793cc35791ec76e1a31134fa0c60a5
Jonathan Yuen <jonwyuen@gmail.com>
2018-05-27 03:01:32 -0700

    header

--- react_redux/auth/client/src/index.js
+++ react_redux/auth/client/src/index.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { Router, Route, IndexRoute, browserHistory } from "react-router";
 
-import App from './components/app';
-import reducers from './reducers';
+import App from "./components/app";
+import reducers from "./reducers";
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+    <Router history={browserHistory}>
+      <Route path="/" component={App} />
+    </Router>
+  </Provider>,
+  document.querySelector(".container")
+);
