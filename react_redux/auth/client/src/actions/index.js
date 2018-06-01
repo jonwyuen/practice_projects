4d14aac838259f3e34bcabbc3d9740c292bcc9a9
Jonathan Yuen <jonwyuen@gmail.com>
2018-06-01 04:15:46 -0700

    auth reducer

--- react_redux/auth/client/src/actions/index.js
+++ react_redux/auth/client/src/actions/index.js
@@ -1,14 +1,33 @@
 import axios from 'axios';
+import { browserHistory } from 'react-router';
+import { AUTH_USER } from "./types";
+
 const ROOT_URL = "http://localhost:3090";
+
+
+/* 
+
+whenever signinUser action creator called, return fn that is auto called by redux thunk middleware
+
+redux thunk middleware call that fn w/ dispatch method
+
+*/
 
 export function signinUser({ email, password }) {
   // redux-thunk allows us to return a fn from action creator
   // fn automatically be called with dispatch method(makes sure action gets sent to all reducers)
-  // redux-thunk allows to dispatch multiple actions inside action creator
+  // redux-thunk allows to dispatch multiple and async actions inside action creator
 
   return function(dispatch) {
     // submit email/pw to server
-    axios.post(`${ROOT_URL}/signin`, { email, password });
+    axios.post(`${ROOT_URL}/signin`, { email, password })
+      .then(response => {
+        dispatch({ type: AUTH_USER });
+        browserHistory.push('/feature');
+      });
+      .catch(() => {
+        
+      });
   }
 
   // if req good
