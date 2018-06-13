8c015962a3cc44b0c917cc15a02199f0df160de0
Jonathan Yuen <jonwyuen@gmail.com>
2018-06-13 00:13:22 -0700

    fetch msg action

--- react_redux/auth/client/src/actions/index.js
+++ react_redux/auth/client/src/actions/index.js
@@ -64,3 +64,15 @@ export function signoutUser() {
     type: UNAUTH_USER
   };
 }
+
+export function fetchMessage() {
+  return function(dispatch) {
+    axios
+      .get(ROOT_URL, {
+        headers: { authorization: localStorage.getItem("token") }
+      })
+      .then(response => {
+        console.log(response);
+      });
+  };
+}
